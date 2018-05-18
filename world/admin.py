9e2188a45def5e5e4831e9502958dd9ceed28c9f
Hiroshi Omata <op.homata@gmail.com>
2018-05-18 21:45:41 +0900

    Add django-leaflet

--- world/admin.py
+++ world/admin.py
@@ -1,5 +1,10 @@
 #from django.contrib import admin
 from django.contrib.gis import admin
 from world.models import Border
+from leaflet.admin import LeafletGeoAdmin
 
-admin.site.register(Border, admin.GeoModelAdmin)
+class BorderAdmin(LeafletGeoAdmin):
+  search_fields = ['n03_001','n03_003','n03_004']
+  list_filter = ('n03_004', )
+
+admin.site.register(Border, BorderAdmin)
